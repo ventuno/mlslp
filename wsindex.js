2bc9d6b4f347ee216bb72a37c791c8651753d8e7
ventuno <ernestom@surfer-172-29-9-141-hotspot.internet-for-guests.com>
2016-02-04 14:25:32 +0100

    Sending test keep alive message for the web socket.

--- wsindex.js
+++ wsindex.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var promise_ize = require('./promise_ize').promise_ize;
 
 var CLIENT_SECRET = process.env.CLIENT_SECRET;
+var WS_KALIVE_TIMEOUT = process.env.WS_KALIVE_TIMEOUT;
 
 var parser = cookieParser(CLIENT_SECRET);
 
@@ -20,14 +21,20 @@ function WSS (server, store) {
     var cookie = cookieParser.signedCookie(decodeURIComponent(ws.upgradeReq.headers.cookie.split('=')[1]), CLIENT_SECRET);
     promise_ize(store.get, [cookie], store).promise.then(function (session) {
       var emailAddress = session.emailAddress;
+      var intervalId = null;
       console.log('websocket connection open by', emailAddress);
       _this._connections[session.emailAddress] = ws;
+
+      intervalId = setInterval(function () {
+        ws.send(JSON.stringify({msg: "kalive"}));
+      }, WS_KALIVE_TIMEOUT);
 
       ws.on("message", function(msg) {
         console.log('websocket msg', msg, 'by:', emailAddress);
       });
 
       ws.on("close", function() {
+        clearInterval(intervalId);
         _this._connections[session.emailAddress] = null;
         console.log('websocket connection closed by', emailAddress);
       });
